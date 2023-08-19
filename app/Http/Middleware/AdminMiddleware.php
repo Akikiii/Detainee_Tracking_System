c6e808ed92343680d538111b23b30377f07140fd
Akikiii <woofmeowie@gmail.com>
2023-08-20 00:35:36 +0800

    Fixed Admin and User middleware
    Made a middleware for admin to separate between a normal user and an admin

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //Checks if admin or login done
+        if(Auth::check()){ 
+            if(Auth::user()->role == '1'){ //Admin == '1' else User == '0'
+                return $next($request);
+            }else{
+                return redirect('/dashboard');
+            }
+        }else{
+            return redirect('/login');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +20,7 @@ Route::get('/', function () {
 });
 
      
-Route::prefix('admin')->middleware('auth')->group(function(){
+Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
     Route::get("Create_Attorney", function(){ return view("Create_Attorney"); });
     });
     
@@ -28,11 +29,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Auth::routes();
 require __DIR__.'/auth.php';
 
