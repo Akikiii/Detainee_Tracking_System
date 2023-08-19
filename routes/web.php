63e909dde9e570537048c7f55856314a4d85fd01
Akikiii <woofmeowie@gmail.com>
2023-08-19 21:21:49 +0800

    Added Roles
    NOT YET FIXED(FIX ROUTE FOR ADMIN)

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+     
+Route::prefix('admin')->middleware('auth')->group(function(){
+    Route::get("Create_Attorney", function(){ return view("Create_Attorney"); });
+    });
+    
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -30,7 +36,3 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-//CREATE ATTORNEY PROFILE
-Route::get("Create_Attorney", function(){
-    return view("Create_Attorney");
-});
