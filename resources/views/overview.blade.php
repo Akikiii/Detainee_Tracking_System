0a8785bce7ff6af49a3088cccf801a4e6091b8d1
Koy <93858393+charleess15@users.noreply.github.com>
2023-12-07 02:41:20 +0800

    OVERVIEW UI UPDATE

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -11,80 +11,90 @@
         <!-- Overview -->
         <div class="col-start-2 my-[2.36rem] mx-[3rem] col-span-4 bg-[#FFFFFF] py-[2.81rem] px-[2.84rem] border-2 border-black mt-[2.38rem]" style="background-image: url('logos/background-with-grid-bw.png'); background-size: contain;">
             <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-                <div class="grid gap-24 row-gap-8 lg:grid-cols-5">
-                    <div class="grid gap-8 lg:col-span-2">
-                        <div>
-                            <p class="mb-2 text-lg font-bold">Public Attorney's Office Mission</p>
-                            <p class="text-gray-700">
-                            By 2023, the Public Attorney's Office, as the principal law office of the government, shall be composed of highly motivated, effective and empowered public servants, consistently responsive to the ever-growing legal needs of the indigents and other qualified persons, utilizing modern facilities, information technology systems and tools, needed for efficient delivery of free legal aid services to promote access to justice, truth and peace.
-                            </p>
+                <div class="flex flex-row justify-center gap-20 py-12">
+                    <div class="bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                        <div class="flex">
+                            <h6 class="mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400">
+                                {{ \App\Models\Detainee::count() }}
+                            </h6>
+                            <div class="flex items-center justify-center rounded-full bg-teal-accent-400 w-7 h-7">
+                            <svg class="text-teal-900 w-7 h-7" stroke="currentColor" viewBox="0 0 52 52">
+                                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                            </svg>
+                            </div>
                         </div>
-                        <div>
-                            <p class="mb-2 text-lg font-bold">Public Attorney's Office Vision</p>
-                            <p class="text-gray-700">
-                            The Public Attorney's Office exists to provide the indigent litigants, the oppressed, marginalized, and underprivileged members of the society free access to courts, judicial and quasi-judicial agencies, by rendering legal services, counselling and assistance in consonance with the Constitutional mandate that "free access to courts shall not be denied to any person by reason of poverty" in order to ensure the rule of law, truth and social justice as components of the country's sustainable development.
-                            </p>
-                        </div>
+                        <p class="mb-2 font-bold md:text-lg">Detainees</p>
+                        <p class="text-gray-700">
+                            Number of Detainees that were added to the Detainee Tracking System.
+                        </p>
                     </div>
-
-                    <div class="grid border divide-y rounded lg:col-span-3 sm:grid-cols-2 sm:divide-y-0 sm:divide-x">
-                        <div class="flex flex-col justify-between p-10">
-
-                            <div>
-                                <p class="text-base font-semibold text-gray-800">
-                                    Detainees
-                                </p>
-                                <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">
-                                    {{ \App\Models\Detainee::count() }}
-                                </p>
-                            </div>
-
-                            <div>
-                                <p class="text-sm font-semibold text-gray-800">
-                                    Detainees with Assigned Attorneys
-                                </p>
-                                <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">
-                                    {{ \App\Models\Counsel_Case_Assignment::whereNotNull('assigned_lawyer')->count() }}
-                                </p>
-                            </div>
-
-                            <div>
-                                <p class="text-sm font-semibold text-gray-800">
-                                    Detainees without Assigned Attorneys
-                                </p>
-                                <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">
-                                    {{ $withoutAttorneysCount  }}
-                                </p>
+                    <div class="bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                        <div class="flex">
+                            <h6 class="mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400">
+                                {{ \App\Models\Counsel_Case_Assignment::whereNotNull('assigned_lawyer')->count() }}
+                            </h6>
+                            <div class="flex items-center justify-center rounded-full bg-teal-accent-400 w-7 h-7">
+                            <svg class="text-teal-900 w-7 h-7" stroke="currentColor" viewBox="0 0 52 52">
+                                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                            </svg>
                             </div>
                         </div>
-
-                        <div class="flex flex-col justify-between p-10">
-                            <div>
-                                <p class="text-base font-semibold text-gray-800">Users</p>
-                                <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">
-                                    {{ \App\Models\User::count() }}
-                                </p>
-                            </div>
-
-                            <div>
-                                <p class="text-base font-semibold text-gray-800">
-                                    Cases
-                                </p>
-                                <p class="text-2xl font-bold text-deep-purple-accent-400">
-                                    {{ \App\Models\Cases::count() }}
-                                </p>
-                            </div>
-
-                            <div>
-                                <p class="text-base font-semibold text-gray-800">
-                                    
-                                </p>
-                                <p class="text-2xl font-bold text-deep-purple-accent-400">
-                                    
-                                </p>
-                            </div>
-                        </div>
+                        <p class="mb-2 font-bold md:text-lg">Detainees with Assigned Attorney</p>
+                        <p class="text-gray-700">
+                            Number of Detainees that has already an assigned Attorney for their pending cases.
+                        </p>
                     </div>
+                    <div class="bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                        <div class="flex">
+                            <h6 class="mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400">
+                                {{ $withoutAttorneysCount  }}
+                            </h6>
+                            <div class="flex items-center justify-center rounded-full bg-teal-accent-400 w-7 h-7">
+                            <svg class="text-teal-900 w-7 h-7" stroke="currentColor" viewBox="0 0 52 52">
+                                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                            </svg>
+                            </div>
+                        </div>
+                        <p class="mb-2 font-bold md:text-lg">Detainees without Assigned Attorney</p>
+                        <p class="text-gray-700">
+                            Number of Detainees without an Attorney for their pending cases.
+                        </p>
+                    </div>
+                </div>
+                <div class="flex flex-row justify-center gap-20 py-12">
+                    <div class="bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                        <div class="flex">
+                            <h6 class="mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400">
+                                {{ \App\Models\User::count() }}
+                            </h6>
+                            <div class="flex items-center justify-center rounded-full bg-teal-accent-400 w-7 h-7">
+                            <svg class="text-teal-900 w-7 h-7" stroke="currentColor" viewBox="0 0 52 52">
+                                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                            </svg>
+                            </div>
+                        </div>
+                        <p class="mb-2 font-bold md:text-lg">Users</p>
+                        <p class="text-gray-700">
+                            Number of Attorneys that has access to the Detainee Tracking System.
+                        </p>
+                    </div>
+                    <div class="bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                        <div class="flex">
+                            <h6 class="mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400">
+                                {{ \App\Models\Cases::count() }}
+                            </h6>
+                            <div class="flex items-center justify-center rounded-full bg-teal-accent-400 w-7 h-7">
+                            <svg class="text-teal-900 w-7 h-7" stroke="currentColor" viewBox="0 0 52 52">
+                                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+                            </svg>
+                            </div>
+                        </div>
+                        <p class="mb-2 font-bold md:text-lg">Cases</p>
+                        <p class="text-gray-700">
+                            Number of Occurring Cases of the Detainees.
+                        </p>
+                    </div>
+                <div>
                 </div>
             </div>
         </div>
