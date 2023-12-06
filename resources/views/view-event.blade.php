ea17c72255c0ebbe4cb143eb2dd3a9ff2d1dd42b
Koy <93858393+charleess15@users.noreply.github.com>
2023-12-07 01:44:52 +0800

    VIEW DETAINEE AND VIEW EVENT UPDATE

--- resources/views/view-event.blade.php
+++ resources/views/view-event.blade.php
@@ -14,7 +14,7 @@
                 <h1 class="text-[1.875rem] uppercase font-bold ibm-plex-mono" style="color: black; text-shadow: 0 0 2px #888888;">EVENT DETAILS</h1>
                 <img class="mt-[1.94rem] mb-[2.31rem]" src="{{ asset('logos/line-bw.png') }}" alt="TVA Line" style="height: 10px;">
 
-                <div class="grid gap-5">
+                <div class="grid">
 
                     <div class="flex flex-col gap-4">
                         <label class="form-label block labelname font-bold">Event Information</label>
@@ -25,12 +25,13 @@
                                     <div class="ml-2">
                                         @if(isset($event))
                                             <p class="text-[1.875rem] mb-3"><strong>Case Title: </strong> {{ $event->case->case_name}}</p>
-                                            <p class="text-left mb-3"><strong>Event Type:</strong> {{ $event->event_type }}</p>
                                             @if ($event->event_type == 'Bail')
+                                                <p class="text-left mb-3"><strong>Event Type:</strong> Bail Hearing</p>
                                                 <p class="text-left mb-3"><strong>Bail Amount: </strong>{{ optional($event->bail)->amount}}</p>
                                                 <p class="text-left mb-3"><strong>Bail Status: </strong>{{ ucfirst($event->bail_confirmation) }}</p>
                                                 <p class="text-left mb-3"><strong>Bail Type: </strong>{{optional($event->bail)->bail_type}}</p>
                                             @elseif ($event->event_type == 'Arraignment')
+                                                <p class="text-left mb-3"><strong>Event Type:</strong> {{ $event->event_type }}</p>
                                                 <p class="text-left mb-3"><strong>Verdict: </strong>
                                                     @if ($event->verdict == 'guilty')
                                                         Guilty
@@ -40,6 +41,10 @@
                                                         No Contest
                                                     @endif
                                                 </p>
+                                            @elseif ($event->event_type == 'Plea')
+                                                <p class="text-left mb-3"><strong>Event Type:</strong> Plea Bargaining</p>
+                                            @else
+                                                <p class="text-left mb-3"><strong>Event Type:</strong> {{ $event->event_type }}</p>
                                             @endif
                                             <p class="text-left mb-3"><strong>Event Date:</strong> {{ $event->event_date }}</p>
                                             <p class="text-left mb-3"><strong>Event Location:</strong> 
@@ -62,32 +67,35 @@
                     </div>
 
                     <!-- Comment Box -->
-                        <div class="col-start-2 my-[2.36rem] mx-[3rem] col-span-4 bg-[#FFFFFF] py-[2.81rem] px-[2.84rem] border-2 border-black mt-[2.38rem]" style="background-image: url('logos/background-with-grid-bw.png'); background-size: contain;">
-                            <!-- Existing code for Live Case header and events table -->
+                    <div class="flex flex-col gap-4">
+                        <div class="flex space-x-4">
+                            <div class="flex flex-row py-4 px-4 w-full leading-tight focus:outline-none focus:border-black relative">
+                                <!-- Comment Box Section -->
+                                <div class="mt-6">
+                                    <h2 class="text-[1.5rem] font-bold ibm-plex-mono" style="color: black;">Comments</h2>
+                                    
+                                    <!-- Comment Form -->
+                                    <form method="post" action="{{ route('post-comment', ['case_id' => $event->case_id, 'event_id' => $event->id]) }}">
+                                        @csrf
+                                        <textarea name="commentText" class="w-full h-20 mt-2 p-2 border border-gray-300 rounded" placeholder="Write your comment here..."></textarea>
+                                        <button type="submit" class="mt-2 px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700">Post Comment</button>
+                                    </form>
 
-                            <!-- Comment Box Section -->
-                            <div class="mt-6">
-                                <h2 class="text-[1.5rem] font-bold ibm-plex-mono" style="color: black;">Comments</h2>
-                                
-                                <!-- Comment Form -->
-                                <form method="post" action="{{ route('post-comment', ['case_id' => $event->case_id, 'event_id' => $event->id]) }}">
-                                    @csrf
-                                    <textarea name="commentText" class="w-full h-20 mt-2 p-2 border border-gray-300 rounded" placeholder="Write your comment here..."></textarea>
-                                    <button type="submit" class="mt-2 px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700">Post Comment</button>
-                                </form>
+                                    <!-- Display Existing Comments -->
+                                    <div class="mt-4">
+                                        <!-- Loop through and display existing comments -->
+                                        @foreach ($comments as $comment)
+                                            <div class="bg-gray-100 p-2 rounded mt-2">
+                                                <strong>{{ optional($comment->user)->name }}</strong>: {{ $comment->commentText }}
+                                            </div>
+                                        @endforeach
 
-                                <!-- Display Existing Comments -->
-                                <div class="mt-4">
-                                    <!-- Loop through and display existing comments -->
-                                    @foreach ($comments as $comment)
-                                        <div class="bg-gray-100 p-2 rounded mt-2">
-                                            <strong>{{ optional($comment->user)->name }}</strong>: {{ $comment->commentText }}
-                                        </div>
-                                    @endforeach
-
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+
                     <div class="flex flex-row justify-end gap-2.5 mt-[2.12rem]">
                         <a href="{{ url()->previous() }}" class="buttonFormat border-2 border-black bg-rgba(165, 42, 42, 0) hover:bg-black text-black hover:text-white font-bold py-4 px-4">BACK TO EVENT TIMELINE</a>
                     </div>
